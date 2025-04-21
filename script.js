6b5a7d9a1e6bc89462fdcc64fb2c4221889eda41
Ashwani Singh <ashwani042004@gmail.com>
2025-04-21 14:36:24 +0530

    Set the 12 hour format

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let clock = document.querySelector(".clock")
 
+const option = {
+    timeZone: 'Asia/kolkata',
+    hours:'2-digit',
+    minutes: '2-digit',
+    second: '2-digit' 
+}
+
+
 function displayTime() {
     let currDate = new Date()
-    clock.innerText = currDate.toLocaleTimeString()
+    clock.innerText = currDate.toLocaleTimeString('en-US',option)
 }
 
 setInterval(displayTime,1000)
